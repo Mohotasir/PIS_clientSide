1ac8fa16cc3ef5eb1096960e5171e4253f444170
mohotasir <mohotasir1036@gmail.com>
2024-05-30 01:36:50 +0600

    edit

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import  { useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import 'react-tooltip/dist/react-tooltip.css'
 import { Tooltip } from 'react-tooltip'
